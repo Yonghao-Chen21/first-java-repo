b71e776ffb656a6bc6ede75a8862bafdda283d55
M Rajesh <m.rajeshrajz@gmail.com>
2020-08-25 11:18:38 +0530

    added simple test cases

--- src/test/java/com/lwl/cj/basics/FundamentalPrograms_1_Test.java
+++ src/test/java/com/lwl/cj/basics/FundamentalPrograms_1_Test.java
@@ -9,7 +9,33 @@ class FundamentalPrograms_1_Test {
 	@Test
 	void addNumbersTest() {
 		FundamentalPrograms_1 obj = new FundamentalPrograms_1();
-		assertEquals(10, obj.addNumbers(5, 5));
+		assertEquals(10, obj.addNumbers(5, 5), "You must add two numbers");
+	}
+
+	@Test
+	void substractNumbersTest() {
+		FundamentalPrograms_1 obj = new FundamentalPrograms_1();
+		assertEquals(10, obj.substractNumbers(20, 10));
+
+	}
+
+	@Test
+	void averageTest() {
+		FundamentalPrograms_1 obj = new FundamentalPrograms_1();
+		assertEquals(5, obj.average(5, 5, 5), 0.02);
+	}
+
+	@Test
+	void swapTest() {
+		FundamentalPrograms_1 obj = new FundamentalPrograms_1();
+		assertEquals("20 and 10", obj.swap(10, 20));
+
+	}
+
+	@Test
+	void simpleInterestTest() {
+		FundamentalPrograms_1 obj = new FundamentalPrograms_1();
+		assertEquals(3600, obj.simpleInterest(50000, 12, 6), 0.02);
 	}
 
 }
--- src/test/java/com/lwl/cj/basics/FundamentalPrograms_2_Test.java
+++ src/test/java/com/lwl/cj/basics/FundamentalPrograms_2_Test.java
@@ -7,8 +7,49 @@ import org.junit.jupiter.api.Test;
 class FundamentalPrograms_2_Test {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void isEven() {
+		FundamentalPrograms_2 obj = new FundamentalPrograms_2();
+		assertEquals(true, obj.isEven(10));
+	}
+
+	@Test
+	void biggestOfTwoTest() {
+		FundamentalPrograms_2 obj = new FundamentalPrograms_2();
+		assertEquals(10, obj.biggest(5, 10));
+	}
+
+	@Test
+	void biggestOfThreeTest() {
+		FundamentalPrograms_2 obj = new FundamentalPrograms_2();
+		assertEquals(30, obj.biggest(10, 15, 30));
+	}
+
+	/*
+	 * If amount 1 - 100 Discount 5% 
+	 * If amount >100 and <= 500 Discount 10% 
+	 * If amount >500 and <= 1000 Discount 12% 
+	 * If amount > 1000 Discount 15%
+	 */
+
+	@Test
+	void getDiscountAmountTest() {
+		FundamentalPrograms_2 obj = new FundamentalPrograms_2();
+		assertEquals(95, obj.getDiscountAmount(100));
+	}
+
+	/* 
+	 * If used units 0 - 100 per unit 3
+	 * > 100 and <= 500 per unit 5 
+	 * > 500 and <= *1000 per unit 8 
+	 * > 1000 per unit 10
+	 * 
+	 * bill amount = minAmount + used units amount
+	 */
+
+	@Test
+	void calElectricityBillTest() {
+		FundamentalPrograms_2 obj = new FundamentalPrograms_2();
+		assertEquals(80, obj.calElectricityBill(50, 30), 0.02);
 	}
 
 }
